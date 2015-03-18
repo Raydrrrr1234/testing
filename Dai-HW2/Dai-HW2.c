13e90fcf283a5f0f4ad9a7b2b7348ed005c58e05
DianeRay <ruikang.dai@gmail.com>
2015-03-17 22:01:47 -0400

    About this homework
    The Makefile makes a mistake that Debug information must done at the .o
    file
    ==============================
    Secondly, I completed debugged the .c file. The program works well.

--- Dai-HW2/Dai-HW2.c
+++ Dai-HW2/Dai-HW2.c
@@ -4,11 +4,10 @@
    DATE:Sat Feb 28 16:54:03 EST 2015
 */
 #include <stdio.h>
-#include <malloc.h>
 #include <errno.h>
-#include <stdlib.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <unistd.h>
 int system(const char *command);
 int makeargv(const char * command, const char *delimiters, char ***argvp);
 void freemakeargv(int argn, char **argv);
@@ -36,6 +35,12 @@ int main (int argc, char *argv) {
 	return 0;
 }
 #endif
+#ifdef DEBUG
+int main(int argn, char **argv) {
+	system("outputIDs");
+	return 0;
+}
+#endif
 int system(const char *command) {
 	int i;
 	// The number of argments in the command
@@ -45,14 +50,76 @@ int system(const char *command) {
 	// The delimiters that seperate the command
 	char delim[] = " \t";
  
-	if((argn = makeargv(command, delim, &argv)) == NULL) {
+	if ((argn = makeargv(command, delim, &argv)) <= 0) {
 		fprintf(stderr, "Failed to construct an argument array\n");
+		return -1;
 	} 
 	
-	if(execvp(program_name,argv) == -1) 
+	if (execvp(argv[0],argv+1) == -1) 
 		errorInfo(errno);
 	// Free memories in heap
 	freemakeargv(argn,argv);
+	return 1;
+}
+void freemakeargv(int argn, char **argv) {
+	if (argv == NULL)
+		return;
+	while (argn != 0)
+		free(argv[--argn]);
+	free(argv);
+}
+int makeargv(const char * command, const char *delimiters, char ***argvp) {
+	// strtok_r thread safety argument
+	char *lasts;
+	// Temporary variables
+	int argn = 0;
+	int error;
+	char *command_t;
+	char **argv;
+	// Input error checking
+	if ((command == NULL) || (delimiters == NULL) || (argvp == NULL)) {
+		errno = EINVAL;
+		return -1;
+	}
+	// copy the command to command_t
+	if ((command_t = (char*)malloc(strlen(command)*sizeof(char))) == NULL) {
+		perror("Not enough memory!!(system function)");
+		return -1;
+	}
+	strcpy(command_t, command);
+	// count the number of tokens in command
+	if (strtok_r(command_t, delimiters,&lasts) != NULL) {
+		argn++;
+		while(strtok_r(NULL, delimiters,&lasts) != NULL) {
+			argn++;
+		}
+	}
+	if (argn == 0) {
+		perror("No command!");
+		return -1;
+	}
+	// Create argument array for ptrs to the tokens	
+	if ((argv = (char**)malloc(sizeof(char*)*argn+1)) == NULL) {
+		perror("Not enough memory!!(system function)");
+		return -1;
+	}
+	else {
+	// Insert pointers to tokens into the argument array 
+		argv[0] = strtok_r(command_t, delimiters,&lasts);
+		for (int i = 1; i < argn; i++) {
+			char *arg = strtok_r(NULL, delimiters,&lasts);
+			if ((argv[i] = (char*)malloc(sizeof(char)*strlen(arg)+1)) == NULL) {
+				printf("Not enough memory!!(system function)");
+				return -1;
+			}
+			strcpy(argv[i],arg);
+		}
+		// put in a finall NULL pointer
+		argv[argn] = NULL;
+	}
+	free(command_t);
+	*argvp = argv;
+	return argn;
 }
 void errorInfo(int error) {
 	switch(error) {
@@ -78,68 +145,4 @@ void errorInfo(int error) {
 		fprintf(stderr, "a component of the image file path prefix is not a directory");
 		break;
 	}
-}
-void freemakeargv(int argn, char **argv) {
-	if (argv == NULL)
-		return;
-	if (*argv != NULL)
-		while(argn != 0)
-			free(argv[--argn]);
-	free(argv);
-}
-int makeargv(const char * command, const char *delimiters, char ***argvp) {
-	int argn = 0;
-	int error;
-	int numtokens;
-	// strtok_r thread safety argument
-	char *lasts;
-	// Input error checking
-	if ((command == NULL) || (delimiters == NULL) || (argvp == NULL)) {
-		errno = EINVAL;
-		return -1;
-	}
-	// Copy the command string
-	if((char *temp = (char *)malloc(sizeof(char)*strlen(command))) == NULL) {
-		printf("Not enough memory!!(system function)");
-		return -1;
-	}
-	// Free the temp clauses seperate tokens
-	{
-		char *temp_for_delete = temp;
-		// count the number of tokens in command
-		strcpy(temp, command);
-		if(strtok_r(temp, delimiters,&lasts) != NULL) {
-			while(strtok_r(NULL, delimiters,&lasts) != NULL) {
-				argn++;
-			}
-		}
-		// Create argument array for ptrs to the tokens	
-		if((char **argv = (char*)malloc(sizeof(char*)*argn+1)) == NULL) {
-			error = errno;
-			free(temp_for_delete);
-			errno = error;
-			printf("Not enough memory!!(system function)");
-			return -1;
-		}
-		if(numtokens == 0)
-			free(temp_for_delete);
-		else {
-			// Insert pointers to tokens into the argument array 
-			strcpy(temp, command);
-			char *program_name = strtok_r(temp, delimiters,&lasts);
-			for(int i = 0; i < argn; i++) {
-				char *arg = strtok_r(NULL, delimiters,&lasts);
-				if((argv[i] = (char*)malloc(sizeof(char)*strlen(arg)+1)) == NULL) {
-					printf("Not enough memory!!(system function)");
-					return -1;
-				}
-				strcpy(argv[i],arg);
-			}
-			// put in a finall NULL pointer
-			argv[argn] = NULL;
-			free(temp_for_delete);
-		}
-	}
-	*argvp = argv;
-	return argn;
 }
